770db1d1b5322d2c6ef91fcf0c43556822b28ac8
恺心 <jimin.wjm@alipay.com>
2017-05-14 03:25:39 +0800

    BC is Done.

--- routes/check.js
+++ routes/check.js
@@ -163,4 +163,17 @@ router.post('/hospital/verify', function (req, res, next) {
     });
 });
 
+router.post('/third/query/check2', function (req, res, next) {
+    BC.verify({"hospitalId": "admin", "hospitalAgree": "admin", "patientId": req.body.patientId, "patientAgree": req.body.verifyCode}, function (err, doc) {
+        if(err == null){
+            console.log("/****************** BC verify single success ***********************/");
+        }
+    });
+    CoreData.find({"_id": req.body._id}, function (err, doc) {
+        if(err == null){
+            res.json(doc);
+        }
+    });
+});
+
 module.exports = router;
